a2928486c255b6da92613e4483b1403368a2867f
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-06-06 20:55:58 -0600

    Initialization of project and Config Tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="bg-gradient-to-r from-Instagram1 to-Instagram2 text-2xl text-BrightRed">Hello World!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        Inter: ['Inter', 'sans-serif'],
+       },
+      colors: {
+        // Primary
+        'LimeGreen': '#1db489',
+        'BrightRed': '#dc414c',
+        'Facebook': '#198ff5',
+        'Twitter': '#1ca0f2',
+        'Instagram1': '#fdc468',
+        'Instagram2': '#df4996',
+        'YouTube': '#c4032a',
+
+        // Dark Theme
+        'ToggleDark1': '#378fe6',
+        'ToggleDark2': '#3eda82',
+        'VeryDarkBlue': '#1e202a',  //(BG)
+        'VeryDarkBlue2': '#1f212e',  //(Top BG Pattern)
+        'DarkDesaturatedBlue': '#252a41',  //(Card BG)
+        'DesaturatedBlue': '#3eda82',  // (Text)
+
+        // Light Theme
+        'ToggleLight': '#aeb3cb',
+        'VeryPaleBlue': '#f5f7ff', //(Top BG Pattern)
+        'LightGrayishBlue': '#f0f2fa', //(Card BG)
+        'DarkGrayishBlue': '#63687e', //(Text)
+        'VeryDarkBlue': '#1e202a', //(Text)
+        
+      },
+    },
+  },
+  plugins: [],
+}
+
