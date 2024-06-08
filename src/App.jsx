173f925c4308207745204c7dce655079a6af3c3f
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-06-07 19:45:36 -0600

    Component Overview

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import iconInstagram from './assets/icon-instagram.svg'
 import iconYouTube from './assets/icon-youtube.svg'
 import iconDown from './assets/icon-down.svg'
 import iconUp from './assets/icon-up.svg'
+import { Overview } from "./Components/Overview/Overview"
 
 
 function App() {
@@ -54,6 +55,74 @@ function App() {
         text="sunscribers"
         today={144}
       />
+
+     <section className="mt-11">
+       <h2 className="text-2xl text-DarkGrayishBlue font-bold pl-5">Overview - Today</h2>
+       <Overview
+         subTitle="page views"
+         img={iconFacebook}
+         number="87"
+         arrow={iconUp}
+         percentage="3"
+         color="text-LimeGreen"
+       />
+       <Overview
+         subTitle="likes"
+         img={iconFacebook}
+         number="52"
+         arrow={iconDown}
+         percentage="2"
+         color="text-BrightRed"
+       />
+       <Overview
+         subTitle="likes"
+         img={iconInstagram}
+         number="5462"
+         arrow={iconUp}
+         percentage="2257"
+         color="text-LimeGreen"
+       />
+       <Overview
+         subTitle="profile views"
+         img={iconInstagram}
+         number="52k"
+         arrow={iconUp}
+         percentage="1375"
+         color="text-LimeGreen"
+       />
+       <Overview
+         subTitle="retweets"
+         img={iconTwitter}
+         number="117"
+         arrow={iconUp}
+         percentage="303"
+         color="text-LimeGreen"
+       />
+       <Overview
+         subTitle="likes"
+         img={iconTwitter}
+         number="507"
+         arrow={iconUp}
+         percentage="553"
+         color="text-LimeGreen"
+       />
+       <Overview
+         subTitle="likes"
+         img={iconYouTube}
+         number="107"
+         arrow={iconDown}
+         percentage="19"
+         color="text-BrightRed"
+       />
+       <Overview
+         subTitle="total views"
+         img={iconYouTube}
+         number="1407"
+         arrow={iconDown}
+         percentage="12"
+         color="text-BrightRed"
+       />
+     </section>
     </>
   )
 }
---
+++ src/Components/Overview/Overview.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Overview = (props) => {
+  return (
+    <section className="w-80 bg-VeryPaleBlue my-5 m-auto shadow-md rounded-md p-5">
+
+      <div className="flex justify-between items-center">
+        <h3 className="capitalize text-DarkGrayishBlue font-bold text-sm">{props.subTitle}</h3>
+        <img src={props.img} alt="icon-logo" />
+      </div>
+
+      <div className="flex justify-between items-center mt-7">
+        <p className="text-3xl text-VeryDarkBlueLight font-bold">{props.number}</p>
+        <i className="flex items-center gap-[3px]">
+          <img src={props.arrow} alt="icon-arrow" />
+          <span className={`${props.color} text-xs font-bold`}>{props.percentage}%</span>
+        </i>
+      </div>
+
+    </section>
+  );
+};
