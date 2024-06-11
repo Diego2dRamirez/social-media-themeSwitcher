aaf40933b63cf1930c07f1cd8b46bec3bc791588
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-06-10 18:48:27 -0600

    Modified README-template

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className="w-full pt-16 p-5 text-DesaturatedBlue md:text-center">
       Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-blue-500">Frontend Mentor</a>.
-      Coded by <a href="https://github.com/Diego2Drm" target="_blank" className="text-blue-500">Diego Ramírez</a>.
+      Coded by <a href="https://github.com/Diego2Drm/social-media-theme-switcher" target="_blank" className="text-blue-500">Diego Ramírez</a>.
     </footer>
 
   );
